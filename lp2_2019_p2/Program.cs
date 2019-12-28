8d0a1a998b1b17035559e565dcaded4156c3641d
FPTheFluffyPawed <n.ito10@hotmail.com>
2019-12-28 19:24:41 +0000

    Add project files.

---
+++ lp2_2019_p2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lp2_2019_p2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
