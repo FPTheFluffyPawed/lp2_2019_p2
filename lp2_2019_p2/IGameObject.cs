075acf464966f27a6c2d94fd9f5bca3f0c3f0fe5
FPTheFluffyPawed <n.ito10@hotmail.com>
2020-01-01 19:35:31 +0000

    Add base project structure

---
+++ lp2_2019_p2/IGameObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lp2_2019_p2
+{
+    interface IGameObject
+    {
+        void Start();
+
+        void Update();
+
+        void Finish();
+    }
+}
